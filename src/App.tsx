47e239c4ba9ae7b52bbcb354dbc68c324e9219f4
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-04-03 20:28:31 +0200

    Integrate TensorFlow Handpose model for hand detection using webcam

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
-
-import Webcam from 'react-webcam';
-
-import {  useRef } from 'react';
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import { useRef } from "react";
 
 function App() {
   const camRef = useRef<Webcam>(null);
-  const canvaRef = useRef<HTMLCanvasElement>(null);
+
+  const handleHandPose = async () => {
+    await tf.setBackend("webgl");
+    await tf.ready();
+
+    const net: handpose.HandPose = await handpose.load();
+    console.log("Handpose model loaded.");
+
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net: handpose.HandPose) => {
+    if (
+      camRef.current &&
+      camRef.current.video &&
+      camRef.current.video.readyState === 4
+    ) {
+      const video = camRef.current.video;
+      const hand = await net.estimateHands(video);
+      console.log(hand);
+    }
+  };
+
+  handleHandPose();
 
   return (
     <>
-      <Webcam
-      audio={false}
-      ref={camRef}
-      screenshotFormat="image/jpeg"
-      style={{
-        width: '60%',
-        height: '60%',
-        position: 'absolute',
-        top: '20%',
-        left: '20%',
-        borderRadius: '15px',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-        border: '2px solidrgb(223, 245, 224)',
-        zIndex: -1,
-      }}
-      />
-       <canvas
-        className="canvas-style"
-        ref={canvaRef}
-      />
-      
+      <Webcam ref={camRef} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

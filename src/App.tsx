5e564f3168c6a14973b74432459e6950aa5710a0
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-04-03 22:03:04 +0200

    modif hand lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
-import { useRef,  } from "react";
+import { useRef } from "react";
 import { drawHand } from "./utiltes";
 import "./App.css";
 
@@ -9,38 +9,35 @@ function App() {
 
   const camRef = useRef<Webcam>(null);
   const canvaRef = useRef<HTMLCanvasElement>(null);
- 
+
   const handleHandPose = async () => {
     try {
       await tf.setBackend("webgl");
       await tf.ready();
 
-    
       const net: handpose.HandPose = await handpose.load();
       console.log("Handpose model loaded.");
 
-     
       setInterval(() => {
         detect(net);
       }, 100);
-
     } catch (error) {
       console.error("error init handpose:", error);
     }
   };
 
   const detect = async (net: handpose.HandPose) => {
+   
     if (
       typeof camRef.current !== "undefined" &&
       camRef.current !== null &&
-      camRef.current.video && camRef.current.video.readyState === 4
+      camRef.current.video &&
+      camRef.current.video.readyState === 4
     ) {
-      
       const video = camRef.current.video;
-        const videoWidth = camRef.current.video?.videoWidth || 0;
-      const videoHeight =camRef.current.video?.videoHeight || 0;
+      const videoWidth = camRef.current.video?.videoWidth || 0;
+      const videoHeight = camRef.current.video?.videoHeight || 0;
 
-     
       if (camRef.current.video) {
         camRef.current.video.width = videoWidth;
       }
@@ -48,22 +45,19 @@ function App() {
         camRef.current.video.height = videoHeight;
       }
 
-      
       if (canvaRef.current) {
         canvaRef.current.width = videoWidth;
         canvaRef.current.height = videoHeight;
       }
 
-      
       const hand = await net.estimateHands(video);
       console.log(hand);
 
-   
       if (canvaRef.current) {
         const ctx = canvaRef.current.getContext("2d");
         if (ctx) {
           drawHand(
-            hand.map(prediction => ({
+            hand.map((prediction) => ({
               ...prediction,
               landmarks: prediction.landmarks.map(([x, y]) => [x, y]),
             })),
@@ -72,27 +66,14 @@ function App() {
         }
       }
     }
-    
-      }
+  };
 
-
-
-
-   handleHandPose();
-
- 
+  handleHandPose();
 
   return (
     <>
-      <Webcam
-        ref={camRef}
-      className="webcam"
-      />
-
-      <canvas
-        ref={canvaRef}
-        className="canvas"
-      />
+      <Webcam ref={camRef} className="webcam" />
+      <canvas ref={canvaRef} className="canvas" />
     </>
   );
 }
--- src/utiltes.ts
+++ src/utiltes.ts
@@ -1,4 +1,3 @@
-
 interface prediction {
   landmarks: [number, number][];
   annotations: {
@@ -6,20 +5,66 @@ interface prediction {
   };
 }
 
-export const drawHand = (predictions: prediction[], ctx: CanvasRenderingContext2D) => {
-  if(predictions.length === 0) return;
-  if (predictions.length > 0) {
-  predictions.forEach(prediction => {
-    const landmarks = prediction.landmarks;
+type fingertypes = {
+  [key: string]: number[];
+};
 
-    for (let i = 0; i < landmarks.length; i++) {
-      const x = landmarks[i][0];
-      const y = landmarks[i][1];
-      ctx.fillStyle = "red";
-      ctx.beginPath();
-      ctx.arc(x, y, 5, 0, Math.PI * 2);
-      ctx.fill();
-    }
-  });
+
+const fingerjoints :fingertypes= {
+  thumb :[0,1,2,3,4],indexFinger :[0,5,6,7,8],
+  middleFinger :[0,9,10,11,12],
+  ringFinger :[0,13,14,15,16],
+  pinkyFinger :[0,17,18,19,20],
 }
+
+export const drawHand = (
+  predictions: prediction[],
+  ctx: CanvasRenderingContext2D
+) => {
+
+  if (predictions.length === 0) return;
+
+  if (predictions.length > 0) {
+
+    predictions.forEach((prediction) => {
+
+      const landmarks = prediction.landmarks;
+
+      for (let j = 0; j < Object.keys(fingerjoints).length; j++) {
+        const finger =Object.keys(fingerjoints)[j];
+       
+        for (let k = 0; k < fingerjoints[finger].length - 1; k++) {
+        
+          const firstJoint = fingerjoints[finger][k];
+          const secondJoint = fingerjoints[finger][k + 1];
+
+          ctx.beginPath();
+          ctx.moveTo(
+            landmarks[firstJoint][0],
+            landmarks[firstJoint][1]
+          );
+          ctx.lineTo(
+            landmarks[secondJoint][0],
+            landmarks[secondJoint][1]
+          );
+          ctx.strokeStyle = "white";
+          ctx.lineWidth = 4;
+          ctx.stroke();
+        }
+      }
+
+
+      for (let i = 0; i < landmarks.length; i++) {
+        const x = landmarks[i][0];
+        const y = landmarks[i][1];
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, Math.PI * 2);
+        ctx.fill();
+      }
+
+    });
+
+  }
+
 };
